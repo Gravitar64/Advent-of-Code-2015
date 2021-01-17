ee07f3652dabeb414dd44b64dca48593c4c4fef3
Gravitar64 <gravitar@web.de>
2021-01-17 12:26:34 +0100

    Optimiert (itertools)

--- Tag_21.py
+++ Tag_21.py
@@ -1,50 +1,41 @@
 from time import perf_counter as pfc
+from itertools import product
 import re
+
 
 def load(file):
   with open(file) as f:
     return f.read()
 
+
 class Status:
-  def __init__(self,cost,damage,armor):
+  def __init__(self, cost, damage, armor):
     self.cost = int(cost)
     self.damage = int(damage)
     self.armor = int(armor)
 
   def __add__(self, other):
-    return Status(self.cost+other.cost, self.damage+other.damage, self.armor + other.armor)
-    
+    return Status(self.cost + other.cost, self.damage + other.damage, self.armor + other.armor)
 
 
 def get_var(items):
-  for wi in range(5):
-    for ai in range(-1, 5):
-      for ri1 in range(-1, 6):
-        for ri2 in range(-1, 6):
-          v = items['w'][wi]
-          if ai > -1: 
-            v += items['a'][ai]
-          if ri1 > -1 and ri1 != ri2:
-            v += items['r1'][ri1]
-          if ri2 > -1 and ri1 != ri2:
-            v += items['r2'][ri2]
-          yield v
+  for w,a,r1,r2 in product(items['w'],items['a'],items['r'],items['r']):
+    yield w+a+r1+r2 if r1 != r2 else w+a
 
 
 def solve(puzzle, shop):
-  items = {}
-  boss = list(map(int, re.findall(r'\d+', puzzle)))
+  boss = Status(*re.findall(r'\d+', puzzle))
   shop = shop.split('\n')
   
-  items['w'] = [Status(*re.findall(r'\d+', shop[i])) for i in range(1,6)]
-  items['a'] = [Status(*re.findall(r'\d+', shop[i])) for i in range(8,13)]
-  items['r1'] = [Status(*re.findall(r'\d+', shop[i])[1:]) for i in range(15,21)]
-  items['r2'] = [Status(*re.findall(r'\d+', shop[i])[1:]) for i in range(15,21)]
-  
+  items = {}
+  items['w'] = [Status(*re.findall(r'\d+', shop[i])) for i in range(1, 6)]
+  items['a'] = [Status(*re.findall(r'\d+', shop[i])) for i in range(8, 13)]+[Status(0,0,0)]
+  items['r'] = [Status(*re.findall(r'\d+', shop[i])[1:]) for i in range(15, 21)]+[Status(0,0,0)]
+    
   min_invest, max_invest = 99999, 0
   for player in get_var(items):
-    runden_boss = boss[0] / max(player.damage - boss[2], 1)
-    runden_player = 100 / max(boss[1]-player.armor, 1)
+    runden_boss = boss.cost // max(player.damage - boss.armor, 1)
+    runden_player = 100 // max(boss.damage - player.armor, 1)
     if runden_player >= runden_boss and player.cost < min_invest:
       min_invest = player.cost
     if runden_player < runden_boss and player.cost > max_invest:
